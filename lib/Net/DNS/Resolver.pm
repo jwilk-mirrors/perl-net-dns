f3bbd9688acc1a4e71894e450fd437ce9badfd1e
Willem Toorop <willem@net-dns.org>
2011-10-31 14:00:30 +0000

    0.67 release candidate 6
    git-svn-id: https://www.net-dns.org/svn/net-dns/release/0.66_06@950 a29f3fcb-687d-e111-8194-001676cd6a66

--- lib/Net/DNS/Resolver.pm
+++ lib/Net/DNS/Resolver.pm
@@ -18,7 +18,7 @@ $VERSION = (qw$LastChangedRevision$)[1];
 		# Keep $win32version up to date with the value of
 		# $Net::DNS::Resolver::Win32::VERSION
 		#
-		my $win32version = 932; # WIN32VERSION - do not remove!
+		my $win32version = 950; # WIN32VERSION - do not remove!
 					#              - used in t/00-version.t
 
 		if ($@ || $Net::DNS::Resolver::Win32::VERSION < $win32version) {
